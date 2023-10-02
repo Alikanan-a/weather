1bff35ce870de86658055ac6b5949f365b33b492
alikanan <a.kanan2222@gmail.com>
2023-10-02 14:01:03 +0330

    add api

--- src/components/weatherApp/WeatherApp.jsx
+++ src/components/weatherApp/WeatherApp.jsx
@@ -8,12 +8,34 @@ import rain_icon from '../assets/rain.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 const WeatherApp = () => {
-  let api_key = "63dcc7f645eb06646daf583a73ad2499";
+  let api_key = "bd5e378503939ddaee76f12ad7a97608";
+
+  const search = async () => {
+    const element =  document.getElementsByClassName("cityInput"); 
+    if(element[0].value === "")
+    {
+      return 0 ; 
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=${api_key}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity");
+    const wind =document.getElementsByClassName("wind-rate");
+    const temprature = document.getElementsByClassName("weather-temp");
+    const location  = document.getElementsByClassName("weather-location");
+    humidity[0].innerHTML  = data.main.humidity ;
+    wind[0].innerHTML = data.wind.speed ; 
+    temprature[0].innerHTML = data.main.temp ;
+    location[0].innerHTML  = data.name;
+
+  }
+
   return (
     <div className='container'>
       <div className="top-bar">
         <input type="text" className='cityInput' placeholder='Search'/>
-        <div className="search-icon">
+        <div className="search-icon" onClick={() => {search()}}>
           <img src={search_icon} alt="" />
         </div>
       </div>
@@ -28,14 +50,14 @@ const WeatherApp = () => {
         <div className="element">
           <img src={humidity_icon} alt=""  className='icon'/>
           <div className="data">
-             <div className="humidity-percent">64%</div>
+             <div className="humidity">64%</div>
              <div className='text'>Humidity</div>
           </div>
         </div>
         <div className="element">
           <img src={wind_icon} alt=""  className='icon'/>
           <div className="data">
-             <div className="humidity-percent">18 km/h</div>
+             <div className="wind-rate">18 km/h</div>
              <div className='text'>Wind Speed</div>
           </div>
         </div>
